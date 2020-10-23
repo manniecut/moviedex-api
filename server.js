8024276c31f4f47444bbfeeb007c0118b39c3027
Mannie Cutean <68138888+manniecut@users.noreply.github.com>
2020-10-23 19:31:45 -0400

    working

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
-
-
-console.log(process.env.API_TOKEN)
+const helmet = require('helmet');
+const cors = require('cors');
+const MOVIES = require('./movies.json');
 
 const app = express()
 
 app.use(morgan('dev'))
+app.use(helmet())
+app.use(cors())
+
 app.use(function validateBearerToken(req, res, next) {
     const apiToken = process.env.API_TOKEN
     const authToken = req.get('Authorization')
@@ -27,15 +30,41 @@ const validGenres = [
     `Thriller`, `War`, `Western`
 ];
 
-function handleGetMovies(req, res) {
-    res.send('HEY');
-}
-app.get('/movie', handleGetMovies)
 
-function handleGetGenres(req, res) {
+
+/****************** GET HANDLING ***************************/
+
+app.get('/movie', function handleGetMovies(req, res) {
+    let response = MOVIES;
+    if (req.query.genre) {
+        response = response.filter(movie =>
+            movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+        )
+    }
+
+    if (req.query.country) {
+        response = response.filter(movie =>
+            movie.country.toLowerCase().includes(req.query.country.toLowerCase())
+        )
+    }
+
+    /*if(req.query.avg_vote) {
+        response = response.filter(movie =>
+            movie.avg_vote.toLowerCase().includes(req.query.genre.toLowerCase())
+        )
+    }*/
+
+    res.json(response)
+})
+
+
+app.get('/genres', function handleGetGenres(req, res) {
     res.json(validGenres)
-}
-app.get('/genres', handleGetGenres)
+})
+
+
+
+/******************* SERVER *****************************/
 
 const PORT = 8000
 
