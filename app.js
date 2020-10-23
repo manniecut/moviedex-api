91ddbafea2ef62497cdc52b0d9f70c3b21218562
Mannie Cutean <68138888+manniecut@users.noreply.github.com>
2020-10-23 18:38:00 -0400

    working validation

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /********************************************************************************************
   Requirements
     +Users can search for Movies by genre, country or avg_vote
-    --The endpoint is GET /movie
+                xxThe endpoint is GET /movie
     --The search options for genre, country, and/or average vote are provided in query string parameters.
     --When searching by genre, users are searching for whether the Movie's genre includes a specified 
         string. The search should be case insensitive.
@@ -11,7 +11,7 @@
         than or equal to the supplied number.
     --The API responds with an array of full movie entries for the search results
 
-    +The endpoint only responds when given a valid Authorization header with a Bearer API token value.
+                xxThe endpoint only responds when given a valid Authorization header with a Bearer API token value.
 
     +The endpoint should have general security in place such as best practice headers and support for CORS.
 
--- server.js
+++ server.js
@@ -1,23 +1,40 @@
-
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+
+
+console.log(process.env.API_TOKEN)
 
 const app = express()
 
 app.use(morgan('dev'))
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+    console.log('validation')
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(401).json({ error: 'Unauthorized request' })
+    }
+    next()
+})
 
-const validGenres = [`Animation`, `Action`, `Adventre`,
-                        `Biography`, `Comedy`, `Crime`,
-                        `Documentary`, `Drama`, `Fantasy`,
-                        `Grotesque`, `History`, `Horror`,
-                        `Musical`, `Romantic`, `Spy`,
-                        `Thriller`, `War`, `Western`]
+const validGenres = [
+    `Animation`, `Action`, `Adventre`,
+    `Biography`, `Comedy`, `Crime`,
+    `Documentary`, `Drama`, `Fantasy`,
+    `Grotesque`, `History`, `Horror`,
+    `Musical`, `Romantic`, `Spy`,
+    `Thriller`, `War`, `Western`
+];
+
+function handleGetMovies(req, res) {
+    res.send('HEY');
+}
+app.get('/movie', handleGetMovies)
 
 function handleGetGenres(req, res) {
     res.json(validGenres)
-
 }
-
 app.get('/genres', handleGetGenres)
 
 const PORT = 8000
