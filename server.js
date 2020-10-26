ec33baa1a43a2df10deceaa72668c37abce8418b
Mannie Cutean <68138888+manniecut@users.noreply.github.com>
2020-10-26 16:44:08 -0400

    preparing for deploy

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ const MOVIES = require('./movies.json');
 
 const app = express()
 
-app.use(morgan('dev'))
+const morganSet = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSet))
+
 app.use(helmet())
 app.use(cors())
 
@@ -18,7 +20,6 @@ app.use(cors())
 app.use(function validateBearerToken(req, res, next) {
     const apiToken = process.env.API_TOKEN
     const authToken = req.get('Authorization')
-    console.log('validation')
     if (!authToken || authToken.split(' ')[1] !== apiToken) {
         return res.status(401).json({ error: 'Unauthorized request' })
     }
@@ -53,8 +54,17 @@ app.get('/movie', function handleGetMovies(req, res) {
 
 /******************* SERVER *****************************/
 
-const PORT = 8000
+app.use((error, req, res, next) => {
+    let response
+    if (process.env.NODE_ENV === 'production') {
+        response = { error: {message: 'server error' }}
+    } else {
+        response = { error }
+    }
+    res.status(500).json(response)
+})
+
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
-    console.log(`Server is listenin' at http://localhost:${PORT}`)
 })
